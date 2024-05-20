3c24294bc84d7cc520c9d75433c62bd5f39487dc
AlbertRossJoh <albertrossj@gmail.com>
2024-05-20 11:17:42 +0200

    Update test_sol.bash

--- test_sol.bash
+++ test_sol.bash
@@ -1,16 +1,34 @@
 #!/bin/bash
 tests=(./data/s*/*.in)
 expected=(./data/s*/*.ans)
+tle=0
+normal=0
 for (( i=0; i<${#tests[*]}; ++i)); do
     filename=${tests[$i]}
     ex=$(cat ${expected[$i]})
-    res=$(python3 ./submissions/accepted/sol.py < $filename)
-    # printf "${res} \n"
-    # printf "${ex} \n"
-
-    if (( $res == $ex )); then
-        printf "Success \n"
+    res1=$(timeout 3s python3 ./submissions/accepted/sol.py < $filename)
+    if (( $? == 124 )); then
+        normal=1
+        res1=100
+        printf "Timelimit exceeded on ${filename}\n"
     else
-        printf "Failed \n"
+        normal=0
+    fi
+    res2=$(timeout 3s python3 ./submissions/time_limit_exceeded/tle.py < $filename)
+    # printf "${res} \n"
+    # printf "${res2} \n"
+    if (( $? == 124 )); then
+        tle=1
+    fi
+
+    if (( $res1 == $ex && $normal == 0 )); then
+        printf "Success: ${filename}\n"
+    else
+        printf "Failed: ${filename}\n"
     fi
 done
+if (( $tle == 1 )); then
+    printf "Successful TLE\n"
+else
+    printf "Failed TLE\n"
+fi
